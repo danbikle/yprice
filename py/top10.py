d242a7df17d2e366efd2ce1f1f7c79b59353fd21
Dan Bikle <danbikle@outlook.com>
2017-07-03 15:40:55 -0700

    more

---
+++ py/top10.py
@@ -0,0 +1,26 @@
+"""
+  top10.py
+
+  This script should get price history of top10.
+  Also it should generate 'unsplit' prices.
+  Demo:
+  ${HOME}/anaconda3/bin/python top10.py
+"""
+
+import glob
+import os
+import chromedriver
+import mlprep
+import pdb
+import time
+
+for tkr_s in ['^GSPC','^RUT','AAPL','AMZN','GOOG','MSFT','DIA','QQQ','SPY','XOM']:
+    print('Busy...')
+    time.sleep(6)
+    print(tkr_s)
+    try:
+      chromedriver.get_csv(tkr_s,'history')
+      mlprep.unsplit(tkr_s)
+    except:
+      print(tkr_s+' problem: get_csv() or unsplit() exception.')
+'bye'
