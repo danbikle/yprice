fc6c75d61b36971938bf8371504149d88087ba71
Dan Bikle <danbikle@outlook.com>
2017-07-03 15:32:26 -0700

    more

--- fflask.bash
+++ fflask.bash
@@ -3,6 +3,11 @@
 # fflask.bash
 
 # This script should start a Flask server.
+# I usually run this script on my laptop rather than heroku.
+
+export SCRIPT=`realpath $0`
+export SCRIPTPATH=`dirname $SCRIPT`
+cd    $SCRIPTPATH
 export FLASK_DEBUG=1
 python fflask.py
 
